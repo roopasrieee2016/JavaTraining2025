ae7eee3aa416cea96056b37693f58f85d47c096e
Bathula Roopasri <80576042+roopasrieee2016@users.noreply.github.com>
2025-06-01 12:31:45 +0530

    Update TrainingApplication.java

--- training/src/main/java/com/java/training/TrainingApplication.java
+++ training/src/main/java/com/java/training/TrainingApplication.java
@@ -8,6 +8,7 @@ public class TrainingApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hai");
+		System.out.println("featureB");
 		SpringApplication.run(TrainingApplication.class, args);
 	}
 
