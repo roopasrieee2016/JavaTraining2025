2bdf14106141c6a222ac4f3744fe8d42556c13f2
roopasrieee2016 <roopasrieee@gmail.com>
2025-06-01 12:58:09 +0530

    featureA

--- training/src/main/java/com/java/training/TrainingApplication.java
+++ training/src/main/java/com/java/training/TrainingApplication.java
@@ -9,6 +9,7 @@ public class TrainingApplication {
 	public static void main(String[] args) {
 		System.out.println("Hai");
 		System.out.println("featureB");
+		System.out.println("featureA");
 		SpringApplication.run(TrainingApplication.class, args);
 	}
 
